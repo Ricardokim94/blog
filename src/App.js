81331a55923ac260f91cc2d7b50e00225f274269
Ricardokim94 <cmrikim@naver.com>
2024-04-30 23:05:08 +0900

    변수와 다른 useState 사용하여 Member 만들어보기

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
-let post = 'Member'
-
+  let post = 'Member'
+  let [mem, a] = useState(['오현호','김창목','유영인']);
+  
   return (
     <div className="App">
       <div className='black-nav'>
-        <h4 style={{color : 'red'}}>OH-COACH CLIMBING CREW</h4>
+        <h4>OH-COACH CLIMBING CREW</h4>
       </div>
-      <h4>{post}</h4>
+      
+      <div>
+        <h4 style={{textAlign : 'left', paddingLeft : '20px'}}>{post}</h4>
+      </div>
+
+      <div className="list">
+        <h4>{mem[0]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{mem[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{mem[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
